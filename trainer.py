efaa6553ae775f583e4e4e3b318ca0afabf3f074
Jihae <62400658+easyssun@users.noreply.github.com>
2024-01-23 11:35:39 +0900

    trainer 구성

--- trainer.py
+++ trainer.py
@@ -1,60 +1,84 @@
-from unet import UNet
-from dataset import CustomDataset
+from utils import *
+from baseline import *
 from torch.utils.data import DataLoader
 from torch.optim import Adam
-from torch.nn import MSELoss
+from torch.nn import MSELoss, L1Loss
 from tqdm import tqdm 
+import numpy as np
 
 if __name__ == "__main__":
     
-    img_dir = "C:\\Users\\82102\\OneDrive - gachon.ac.kr\\바탕 화면\\컴퓨터비전 스터디\\data"
-    train_file = "train.csv"
-    val_file = "val.csv"
-    test_file = "test.csv"
-    
+    ab_path = "ab/ab/ab1.npy"
+    l_path = "l/gray_scale.npy"
+
+    ab_df = np.load(ab_path)[0:10000]
+    L_df = np.load(l_path)[0:10000]
+
     epoch = 10
     learning_rate = 0.0001
 
     # 1) dataloader 인스턴스화 (불러오기)
-    train_data = CustomDataset(train_file, img_dir)
-    val_data = CustomDataset(val_file, img_dir)
-    test_data = CustomDataset(test_file, img_dir)
-    train_dataloader = DataLoader(train_data, batch_size=8, shuffle=True)
-    val_dataloader = DataLoader(val_data, batch_size=8, shuffle=True)
-    test_dataloader = DataLoader(test_data, batch_size=8, shuffle=True)
+    # Prepare the Datasets
+    train_dataset = ImageColorizationDataset(dataset = (L_df[:6000], ab_df[:6000]))
+    val_dataset = ImageColorizationDataset(dataset = (L_df[6000:8000], ab_df[6000:8000]))
+    test_dataset = ImageColorizationDataset(dataset = (L_df[8000:], ab_df[8000:]))
+
+    # Build DataLoaders
+    train_loader = DataLoader(dataset=train_dataset, batch_size=1, shuffle = True, pin_memory = True)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=1, shuffle = False, pin_memory = True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle = False, pin_memory = True)
 
     # 2) 모델 인스턴스화
     model = UNet()
 
     # 3) Optimizer, loss function 인스턴스화
-    optimzer = Adam(model.parameters(), lr=learning_rate)
-    loss_fn = MSELoss()
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    
+    loss_fn = L1Loss()
 
     # 4) 데이터로더로 데이터 불러와서 모델에 입력
     for e in tqdm(range(epoch), desc="Epoch"):
+        model.train()  # Set the model to training mode
         train_loss = 0
-        val_loss = 0
 
-        for d in tqdm(train_dataloader, desc="train loader"):
+        for d in tqdm(train_loader, desc="Train Loader"):
             noise_img, gt = d
-            # print(noise_img.dtype)
-            # print(gt.dtype)
             out = model(noise_img)
             
             loss = loss_fn(out, gt)
             
-            train_loss += loss
-            optimzer.zero_grad()
+            train_loss += loss.item()
+            optimizer.zero_grad()
             loss.backward()
-            optimzer.step()
+            optimizer.step()
 
-        print("Train loss: ", train_loss)
-        
-        for d in tqdm(val_dataloader):
+        avg_train_loss = train_loss / len(train_loader)
+        print("Train loss: ", avg_train_loss)
+
+        model.eval()  # Set the model to evaluation mode
+        val_loss = 0
+
+        with torch.no_grad():  # No gradient computation in validation phase
+            for d in tqdm(val_loader, desc="Validation Loader"):
+                noise_img, gt = d
+                out = model(noise_img)
+                val_loss += loss_fn(out, gt).item()  
+
+        avg_val_loss = val_loss / len(val_loader)
+        print("Validation loss: ", avg_val_loss)
+
+        model.train()  # Set the model back to training mode
+
+    # Testing phase
+    model.eval()  # Set the model to evaluation mode
+    test_loss = 0
+
+    with torch.no_grad():  # No gradient computation in testing phase
+        for d in tqdm(test_loader, desc="Testing Loader"):
             noise_img, gt = d
             out = model(noise_img)
-            val_loss += loss_fn(noise_img, gt)  
-            
-        print("Val loss: ", val_loss)
-        
+            test_loss += loss_fn(out, gt).item()
+
+    avg_test_loss = test_loss / len(test_loader)
+    print("Test loss: ", avg_test_loss)
             
--- utils.py
+++ utils.py
@@ -1,55 +1,180 @@
+# import os
+# from pathlib import Path
+# import glob
+# import time
+# import numpy as np
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+# from tqdm import tqdm
+
+# import PIL
+# from PIL import Image
+# from skimage.color import rgb2lab, lab2rgb
+
+# import torch
+# from torch import nn, optim
+# from torchvision import transforms
+# from torchvision.models.resnet import resnet18
+# from torchvision.models.vgg import vgg19
+# from torch.utils.data import Dataset, DataLoader
+
+# image_size_1 = 256
+# image_size_2 = 256
+
+# class ImageDataset(Dataset):
+#     ''' 
+    
+#     Class that deals with the Data Loading and preprocessing steps such as image resizing, data augmentation (horizontal flip) 
+#     and conversion of RGB image to LAB color space with standardization.
+    
+#     '''
+    
+#     def __init__(self,paths,train = True):
+#         if train == True:
+#             self.transforms = transforms.Compose([transforms.Resize((image_size_1,image_size_2)),
+#                                                  transforms.RandomHorizontalFlip()]) # Basic Data Augmentation
+#         elif train == False:
+#             self.transforms = transforms.Compose([transforms.Resize((image_size_1,image_size_2))])
+            
+#         self.train = train
+#         self.paths = paths
+        
+#     def __len__(self):
+#         return len(self.paths)
+    
+#     def __getitem__(self,idx):
+#         img = Image.open(self.paths[idx]).convert("RGB")
+#         img = self.transforms(img)
+#         img = np.array(img)
+#         lab = rgb2lab(img).astype("float32")
+#         lab = transforms.ToTensor()(lab)
+#         L = lab[[0],...]/50 - 1 # Standardizing L space
+#         ab = lab[[1,2],...]/128 # Standardizing ab space
+        
+#         return {'L': L,
+#                 'ab': ab}
+    
 import os
 from pathlib import Path
+
+# Import glob to get the files directories recursively
 import glob
+
+# Import Garbage collector interface
+import gc 
+
+# Import OpenCV to transforme pictures
+import cv2
+
+# Import Time
 import time
+
+# import numpy for math calculations
 import numpy as np
+
+# Import pandas for data (csv) manipulation
+import pandas as pd
+
+# Import matplotlib for plotting
 import matplotlib.pyplot as plt
-%matplotlib inline
-from tqdm import tqdm
+import matplotlib
+import matplotlib
+matplotlib.use('TkAgg')  # You can replace 'TkAgg' with another backend if you prefer
+
+# matplotlib.style.use('fivethirtyeight') 
+# %matplotlib inline
 
 import PIL
 from PIL import Image
 from skimage.color import rgb2lab, lab2rgb
 
+import pytorch_lightning as pl
+
+# Import pytorch to build Deel Learling Models 
 import torch
 from torch import nn, optim
 from torchvision import transforms
-from torchvision.models.resnet import resnet18
-from torchvision.models.vgg import vgg19
 from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
+from torchvision import models
+from torch.nn import functional as F
+import torch.utils.data
+from torchvision.models.inception import inception_v3
+from scipy.stats import entropy
 
-image_size_1 = 256
-image_size_2 = 256
+# from torchsummary import summary
 
-class ImageDataset(Dataset):
-    ''' 
+# Import tqdm to show a smart progress meter
+from tqdm import tqdm
+
+# Import warnings to hide the unnessairy warniings
+import warnings
+warnings.filterwarnings('ignore')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+ab_path = "ab/ab/ab1.npy"
+l_path = "l/gray_scale.npy"
+
+ab_df = np.load(ab_path)[0:10000]
+L_df = np.load(l_path)[0:10000]
+dataset = (L_df,ab_df )
+# gc.collect()
+
+def lab_to_rgb(L, ab):
+    """
+    Takes an image or a batch of images and converts from LAB space to RGB
+    """
+    L = L  * 100
+    ab = (ab - 0.5) * 128 * 2
+    Lab = torch.cat([L, ab], dim=2).numpy()
+    rgb_imgs = []
+    for img in Lab:
+        img_rgb = lab2rgb(img)
+        rgb_imgs.append(img_rgb)
+    return np.stack(rgb_imgs, axis=0)
+
+# img = np.zeros((224,224,3))
+# img[:,:,0] = L_df[0]
+# plt.imshow(lab_to_rgb(img,ab_df[0]))
+
+plt.figure(figsize=(30,30))
+for i in range(1,16,2):
+    print(i)
+    plt.subplot(4,4,i)
+    img = np.zeros((224,224,3))
+    img[:,:,0] = L_df[i]
+    plt.title('B&W')
+    plt.imshow(lab2rgb(img))
     
-    Class that deals with the Data Loading and preprocessing steps such as image resizing, data augmentation (horizontal flip) 
-    and conversion of RGB image to LAB color space with standardization.
+    plt.subplot(4,4,i+1)
+    img[:,:,1:] = ab_df[i]
+    img = img.astype('uint8')
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2RGB)
+    plt.title('Colored')
+    plt.imshow(img)
+plt.show()
     
-    '''
+class ImageColorizationDataset(Dataset):
+    ''' Black and White (L) Images and corresponding A&B Colors'''
+    def __init__(self, dataset, transform=None):
+        '''
+        :param dataset: Dataset name.
+        :param data_dir: Directory with all the images.
+        :param transform: Optional transform to be applied on sample
+        '''
+        self.dataset = dataset
+        self.transform = transform
     
-    def __init__(self,paths,train = True):
-        if train == True:
-            self.transforms = transforms.Compose([transforms.Resize((image_size_1,image_size_2)),
-                                                 transforms.RandomHorizontalFlip()]) # Basic Data Augmentation
-        elif train == False:
-            self.transforms = transforms.Compose([transforms.Resize((image_size_1,image_size_2))])
-            
-        self.train = train
-        self.paths = paths
-        
     def __len__(self):
-        return len(self.paths)
+        return len(self.dataset[0])
     
-    def __getitem__(self,idx):
-        img = Image.open(self.paths[idx]).convert("RGB")
-        img = self.transforms(img)
-        img = np.array(img)
-        lab = rgb2lab(img).astype("float32")
-        lab = transforms.ToTensor()(lab)
-        L = lab[[0],...]/50 - 1 # Standardizing L space
-        ab = lab[[1,2],...]/128 # Standardizing ab space
+    def __getitem__(self, idx):
+        L = np.array(dataset[0][idx]).reshape((224,224,1))
+        L = transforms.ToTensor()(L)
         
-        return {'L': L,
-                'ab': ab}
+        ab = np.array(dataset[1][idx])
+        ab = transforms.ToTensor()(ab)
+
+        return ab, L
+    
