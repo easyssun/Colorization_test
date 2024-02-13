f0db3d2143f96b696434f8a0586dce7c8e4ecc8a
Jihae <62400658+easyssun@users.noreply.github.com>
2024-02-13 21:55:16 +0900

    Update baseline.py
    added UNET_CBAM class

--- baseline.py
+++ baseline.py
@@ -66,3 +66,60 @@ class UNet(nn.Module):
         out = self.conv_last(x)
         
         return out
+    
+
+class UNet_CBAM(nn.Module):
+
+    def __init__(self, output_channels=2):
+        super().__init__()
+                
+        self.dconv_down1 = double_conv(1, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)        
+        
+        self.cbam1 = CBAM(64, 16)
+        self.cbam2 = CBAM(128, 16)
+        self.cbam3 = CBAM(256, 16)
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
+        
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+        
+        self.conv_last = nn.Conv2d(64, output_channels, 1)
+        
+        
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+        
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)   
+        
+        x = self.dconv_down4(x)
+        
+        x = self.upsample(x)        
+        x = torch.cat([x, self.cbam3(conv3)], dim=1)
+        # x = torch.cat([x, conv3], dim=1)
+
+        x = self.dconv_up3(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, self.cbam2(conv2)], dim=1)       
+        # x = torch.cat([x, conv2], dim=1)
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, self.cbam1(conv1)], dim=1)   
+        # x = torch.cat([x, conv1], dim=1)
+        
+        x = self.dconv_up1(x)
+        
+        out = self.conv_last(x)
+        
+        return out
