d2d472a7ddfa3e01f54f21b4f5ea03da8ff941c4
Jihae <62400658+easyssun@users.noreply.github.com>
2024-02-15 21:32:38 +0900

    Add visualize function for GAN

--- inference.py
+++ inference.py
@@ -267,16 +267,133 @@ def visualize_compare(model1, model2, epoch, name=None):
             os.makedirs(f"results/default")
         plt.savefig(f"results/default/validation_{epoch}.png")    
 
+
+def visualize_unet_gan(model, epoch, current_datetime=None):
+
+    plt.figure(figsize=(50,30))
+    for i in range(1,15,3):
+
+        # Original Grayscale image
+        plt.subplot(5,3,i)
+        img = np.zeros((224,224,3))
+        img[:,:,0] = L_df[i]
+        plt.title('B&W')
+        plt.imshow(lab2rgb(img))
+        
+        # Original Colored image
+        plt.subplot(5,3,i+1)
+        img[:,:,1:] = ab_df[i]
+        img = img.astype('uint8')
+        img = cv2.cvtColor(img, cv2.COLOR_LAB2RGB)
+        plt.title('Colored')
+        plt.imshow(img)
+
+        # Predicted image
+        plt.subplot(5,3,i+2)
+        L = np.array(L_df[i]).reshape((224,224,1))
+        L = transforms.ToTensor()(L)
+        L = torch.unsqueeze(L, 0)
+        L = L.to(device)
+        with torch.no_grad():  # No gradient computation in validation phase
+            L = L.to(device)
+            out, _ = model(L)
+
+        out = out.cpu()    
+
+        out = out[0].permute(1,2,0)
+        
+        precited_img = np.zeros((224,224,3))
+        precited_img[:,:,0] = L_df[i]
+        precited_img[:,:,1:] = out * 255
+        precited_img = precited_img.astype('uint8')
+        precited_img = cv2.cvtColor(precited_img, cv2.COLOR_LAB2RGB)
+        plt.title('Predicted')
+        plt.imshow(precited_img)
+
+    if current_datetime != None:
+        if os.path.exists(f"results/{current_datetime}") == False:
+            os.makedirs(f"results/{current_datetime}")
+        plt.savefig(f"results/{current_datetime}/epoch_{epoch}.png")                                                           
+    else:
+        if os.path.exists(f"results/default") == False:
+            os.makedirs(f"results/default")
+        plt.savefig(f"results/default/epoch_{epoch}.png")  
+
+    # validation set에 대한 결과를 저장
+    plt.figure(figsize=(50,30))
+    for i in range(1,15,3):
+
+        # Original Grayscale image
+        plt.subplot(5,3,i)
+        img = np.zeros((224,224,3))
+        img[:,:,0] = L_df[600+i]
+        plt.title('B&W')
+        plt.imshow(lab2rgb(img))
+        
+        # Original Colored image
+        plt.subplot(5,3,i+1)
+        img[:,:,1:] = ab_df[600+i]
+        img = img.astype('uint8')
+        img = cv2.cvtColor(img, cv2.COLOR_LAB2RGB)
+        plt.title('Colored')
+        plt.imshow(img)
+
+        # Predicted image
+        plt.subplot(5,3,i+2)
+        L = np.array(L_df[600+i]).reshape((224,224,1))
+        L = transforms.ToTensor()(L)
+        L = torch.unsqueeze(L, 0)
+        L = L.to(device)
+        with torch.no_grad():  # No gradient computation in validation phase
+            L = L.to(device)
+            out, _ = model(L)
+
+        out = out.cpu()    
+
+        out = out[0].permute(1,2,0)
+        
+        precited_img = np.zeros((224,224,3))
+        precited_img[:,:,0] = L_df[600+i]
+        precited_img[:,:,1:] = out * 255
+        precited_img = precited_img.astype('uint8')
+        precited_img = cv2.cvtColor(precited_img, cv2.COLOR_LAB2RGB)
+        plt.title('Predicted')
+        plt.imshow(precited_img)
+ 
+
+    if current_datetime != None:
+        if os.path.exists(f"results/{current_datetime}") == False:
+            os.makedirs(f"results/{current_datetime}")
+        plt.savefig(f"results/{current_datetime}/val_epoch_{epoch}.png")                                                           
+    else:
+        if os.path.exists(f"results/default") == False:
+            os.makedirs(f"results/default")
+        plt.savefig(f"results/default/val_epoch_{epoch}.png")                                                    
+
 model1 = UNet().to(device)
-model2 = UNet_CBAM().to(device)
+model2 = UNet_pos_enc().to(device)
 # # model pt file load
-epoch = 600
+epoch = 550
 current_datetime1 = "20240213_150951"
-current_datetime2 = "20240213_173918"
+# current_datetime2 = "20240213_173918"
+current_datetime2 = "20240213_220403"
 model1.load_state_dict(torch.load(f"models/{current_datetime1}/model_{epoch}.pt"))
 model2.load_state_dict(torch.load(f"models/{current_datetime2}/model_{epoch}.pt"))
 
 # visualize(model, epoch=99)
 # visualize(model1, epoch, current_datetime1)
 # visualize(model2, epoch, current_datetime2)
-visualize_compare(model1, model2, epoch, name="baseline_vs_cbam")
+visualize_compare(model1, model2, epoch, name="baseline_vs_pos_enc_intermediate") 
+
+
+# model =  UNet_GAN().to(device)
+# epoch = 300
+# current_datetime = "20240215_115851"
+# model.load_state_dict(torch.load(f"models/{current_datetime}/model_{epoch}.pt"))
+# visualize_unet_gan(model, epoch, current_datetime)
+
+
+# visualize(model, epoch=99)
+# visualize(model1, epoch, current_datetime1)
+# visualize(model2, epoch, current_datetime2)
+# visualize_compare(model1, model2, epoch, name="baseline_vs_pos_enc_intermediate") 
